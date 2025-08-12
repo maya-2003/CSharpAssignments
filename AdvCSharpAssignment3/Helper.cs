0b243388e5009416efd4b2683e558124cb8ff2ff
maya-2003 <maya.elb2003@gmail.com>
2025-08-12 10:50:21 +0300

    Add project files.

---
+++ AdvCSharpAssignment3/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvCSharpAssignment3
+{
+    internal class Helper
+    {
+
+        #region 1. implement a function to reverse the elements of a queue using a stack.Given a Queue
+        public static void ReverseQueue<T>(Queue<T> initQueue)
+        {
+            Stack<T> revSatck = new Stack<T>();
+
+            while (initQueue.Count > 0)
+            {
+                revSatck.Push(initQueue.Dequeue());
+            }
+
+            while (revSatck.Count > 0)
+            {
+                initQueue.Enqueue(revSatck.Pop());
+            }
+        }
+        #endregion
+    }
+}
---
+++ AdvCSharpAssignment3/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace AdvCSharpAssignment3
+{
+    internal class Program
+    {
+
+        #region 2. Implement a function to check if a string of parentheses is balanced using a stack.
+        public static bool CheckParenthesesBalance(String str) { 
+            Stack<char> charsSatck = new Stack<char>();
+            foreach (char element in str.ToCharArray()) {
+                if (element == '(' || element == '{' || element == '[')
+                {
+                    charsSatck.Push(element);
+                }
+
+                else if (element == ')' && charsSatck.Count != 0 && (char)charsSatck.Peek() == '(')
+                {
+                    charsSatck.Pop();
+                }
+                else if (element == '}' && charsSatck.Count != 0 && (char)charsSatck.Peek() == '{')
+                {
+                    charsSatck.Pop();
+                }
+                else if (element == ']' && charsSatck.Count != 0 && (char)charsSatck.Peek() == '[')
+                {
+                    charsSatck.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return charsSatck.Count == 0;
+        }
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            #region 1. implement a function to reverse the elements of a queue using a stack.Given a Queue
+            Queue<double> initQueue = new Queue<double>();
+            initQueue.Enqueue(5.6);
+            initQueue.Enqueue(4.2);
+            initQueue.Enqueue(8.3);
+            Console.Write("Original queue: ");
+            foreach (var item in initQueue)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            Helper.ReverseQueue(initQueue);
+            Console.Write("Reversed queue: ");
+            foreach (var item in initQueue)
+                Console.Write(item + " ");
+            Console.WriteLine();
+            #endregion
+
+            #region 2. Implement a function to check if a string of parentheses is balanced using a stack.
+
+            String str = "([{}]";
+            if (CheckParenthesesBalance(str))
+            {
+                Console.WriteLine("Balanced");
+            }
+            else
+            {
+                Console.WriteLine("Not balanced");
+            }
+            
+            #endregion
+
+        }
+    }
+}
