9e4217fdb99c9c8b493bc6ffacc6ac177edd9989
maya-2003 <maya.elb2003@gmail.com>
2025-09-16 13:58:43 +0300

    Add project files.

---
+++ MappingInheritance/Data/MyCompanyContext.cs
@@ -0,0 +1,56 @@
+﻿using MappingInheritance.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MappingInheritance.Data
+{
+    internal class MyCompanyContext : DbContext
+    {
+        public MyCompanyContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-UDFSA0T ; Database= InheritanceCompany ;Trusted_Connection=True ; TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+           
+            //modelBuilder.Entity<FullTimeEmployee>()
+            //    .HasBaseType(typeof(Employee));
+            //modelBuilder.Entity<PartTimeEmployee>()
+            //    .HasBaseType(typeof(Employee));
+
+            //modelBuilder.Entity<Employee>()
+            //            .HasDiscriminator<string>("EmpType")
+            //            .HasValue<FullTimeEmployee>("FTE")
+            //            .HasValue<PartTimeEmployee>("PTE");
+
+            modelBuilder.Entity<FullTimeEmployee>().ToTable("FullTimeEmployee");
+            modelBuilder.Entity<PartTimeEmployee>().ToTable("PartTimeEmployee");
+        }
+
+        #region TPCT
+        //public DbSet<FullTimeEmployee> FullTimeEmployees { get; set; }
+        //public DbSet<PartTimeEmployee> PartTimeEmployees { get; set; }
+        #endregion
+
+        #region TPH
+        //public DbSet<Employee> Employees { get; set; }
+        //public DbSet<FullTimeEmployee> FullTimeEmployees { get; set; }
+        //public DbSet<PartTimeEmployee> PartTimeEmployees { get; set; }
+        #endregion
+
+        #region TPT
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<FullTimeEmployee> FullTimeEmployees { get; set; }
+        public DbSet<PartTimeEmployee> PartTimeEmployees { get; set; }
+        #endregion
+    }
+}
---
+++ MappingInheritance/Program.cs
@@ -0,0 +1,49 @@
+﻿using MappingInheritance.Data;
+using MappingInheritance.Data.Models;
+
+namespace MappingInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using MyCompanyContext myCompanyContext = new MyCompanyContext();
+            var emp01 = new FullTimeEmployee()
+            {
+                Name = "Rana",
+                Age = 20,
+                Address = "Mansoura",
+                Salary = 3_000,
+                StartDate = DateTime.Now,
+            };
+
+            var emp02 = new PartTimeEmployee()
+            {
+                Name = "Mai",
+                Age = 30,
+                Address = "Giza",
+                CountOfHrs = 15,
+                HourRate = 250
+            };
+
+
+            //myCompanyContext.FullTimeEmployees.Add(emp01);
+            //myCompanyContext.PartTimeEmployees.Add(emp02);
+            //myCompanyContext.SaveChanges();
+
+            //var fullTimeEmp = myCompanyContext.FullTimeEmployees.FirstOrDefault();
+            //if(fullTimeEmp is not null)
+            //{
+            //    Console.WriteLine(fullTimeEmp.Name);
+            //}
+            var res = myCompanyContext.FullTimeEmployees.ToList();
+            if (res is not null)
+            {
+                foreach (var item in res)
+                {
+                    Console.WriteLine($"{item.Name} 11 {item.Salary}");
+                }
+            }
+        }
+    }
+}
