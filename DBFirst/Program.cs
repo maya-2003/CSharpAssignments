3a8b594743f7c8776008eabe0145c86ce67a9ce2
maya-2003 <maya.elb2003@gmail.com>
2025-09-16 13:54:25 +0300

    Add project files.

---
+++ DBFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using DBFirst.Models;
+
+namespace DBFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using NorthwindContext context = new NorthwindContext();
+            var res = context.Procedures.SelectAllCategoriesAsync().Result;
+            foreach (var item in res)
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+
+            NorthwindContextProcedures contextProcedures =  new NorthwindContextProcedures(context);
+            var res2 = contextProcedures.SalesByCategoryAsync("Beverages", "2018").Result;
+            foreach (var item in res2)
+            {
+                Console.WriteLine($"{item.ProductName} - {item.TotalPurchase}");
+            }
+        }
+    }
+}
